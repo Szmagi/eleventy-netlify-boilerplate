8af69c99ec593fef34c14b6b3418235b88e55d04
No Name <wpomatis25@gmail.com>
2025-04-12 18:28:02 +0000

    Updated 3 files via CloudCannon.

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,1 +1,4 @@
-// Add your inline JS here
+// Funkcja otwierająca menu po kliknięciu
+document.getElementById('menu-btn').addEventListener('click', function() {
+  document.getElementById('menu').classList.toggle('open');
+});// Add your inline JS here
