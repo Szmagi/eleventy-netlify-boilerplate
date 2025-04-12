1d1ef80fddc5b3915d5fdb57e86fd10ce32aaac6
No Name <wpomatis25@gmail.com>
2025-04-12 21:16:24 +0000

    Updated 1 file via CloudCannon.

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,18 +1,16 @@
-// Funkcja do otwierania i zamykania menu
+// Funkcja do otwierania i zamykania menu (hamburger)
 document.getElementById('menu-toggle').addEventListener('click', function() {
-  var menu = document.getElementById('menu');  // Pobierz element menu
+  var menu = document.getElementById('menu');
   menu.classList.toggle('show');  // Dodaj lub usuń klasę 'show'
 });
-// Funkcja obsługująca rozwijanie menu po kliknięciu
-document.getElementById('menu-btn').addEventListener('click', function() {
-    document.getElementById('menu').classList.toggle('open');
-});
 
-// Funkcja do aktywowania rozwijania menu po najechaniu na przycisk
-document.querySelector('.navbar li').addEventListener('mouseenter', function() {
-    document.querySelector('.submenu').classList.add('open');
-});
+// Funkcja do rozwijania submenu po najechaniu
+document.querySelectorAll('.navbar li').forEach(function(item) {
+  item.addEventListener('mouseenter', function() {
+    this.querySelector('.submenu').classList.add('open');
+  });
 
-document.querySelector('.navbar li').addEventListener('mouseleave', function() {
-    document.querySelector('.submenu').classList.remove('open');
+  item.addEventListener('mouseleave', function() {
+    this.querySelector('.submenu').classList.remove('open');
+  });
 });
